d19677a1c4c31e1659fd97e01331f390df4aff05
Igor <igorvgoncharov@mail.ru>
2023-07-04 23:38:47 +0300

    tasks in lesson2 added

---
+++ Task2.java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Введите значение на которое будем делить: ");
+            int d = Integer.parseInt(scanner.nextLine());
+            System.out.print("Введите индекс элемента: ");
+            int i = Integer.parseInt(scanner.nextLine());
+            double catchedRes1 = intArray[i] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка. Введено не число.");
+        } catch (ArithmeticException e){
+            System.out.println("Ошибка. На ноль делить нельзя.");
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Ошибка. Такого индекса нет.");
+        }
+
+    }
+
+}
---
+++ task4.java
@@ -0,0 +1,16 @@
+﻿//Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. 
+//Пользователю должно показаться сообщение, что пустые строки вводить нельзя.
+
+import java.util.Scanner;
+
+public class task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String inputed_text = scanner.nextLine();
+        if (inputed_text.length() == 0){
+            throw new RuntimeException("Пустые строки вводить нельзя");
+        }
+        System.out.print("Введенн следующий текст: " + inputed_text);
+    }
+}
